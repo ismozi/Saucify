1d07b7f585b068346e7a2be821a05dd3bdbded0f
ismozi <61505183+ismozi@users.noreply.github.com>
2022-08-08 22:52:45 -0400

    Started the UI of the feed page

--- Saucify/Saucify/saucify/lib/screens/MainPage.dart
+++ Saucify/Saucify/saucify/lib/screens/MainPage.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:external_app_launcher/external_app_launcher.dart';
 import 'package:flutter/material.dart';
+import 'package:saucify/screens/FeedPage.dart';
 import 'package:saucify/screens/LibraryScreen.dart';
 import 'package:saucify/screens/SearchPage.dart';
 import 'package:saucify/screens/StatsPage.dart';
@@ -12,6 +13,7 @@ import 'package:tuple/tuple.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../app/app.locator.dart';
+import '../widgets/CustomFloatingActionButtonLocation .dart';
 
 class MainPage extends StatefulWidget {
 
@@ -24,11 +26,14 @@ class MainPageState extends State<MainPage> {
   bool isStatsActive = true;
   bool isLibraryActive = false;
   bool isProfileActive = false;
+  bool isFeedActive = false;
 
   Container container = Container(color:Color.fromARGB(255, 41, 41, 41));
   LibraryScreen libScreen = LibraryScreen();
   StatsPage statsPage = StatsPage();
   SearchPage searchPage = SearchPage();
+  FeedPage feedPage = FeedPage();
+
   dynamic activeScreen;
 
   @override
@@ -40,21 +45,33 @@ class MainPageState extends State<MainPage> {
   void setPage(int index) {
     if (index == 0) {
       setState(() {
+        activeScreen = feedPage;
+        isFeedActive = true;
+        isStatsActive =  false;
+        isLibraryActive = false;
+        isProfileActive = false;
+      });
+    }
+    else if (index == 1) {
+      setState(() {
         activeScreen = statsPage;
+        isFeedActive = false;
         isStatsActive =  true;
         isLibraryActive = false;
         isProfileActive = false;
       });
-    } else if (index == 1) {
+    } else if (index == 2) {
       setState(() {
         activeScreen = libScreen;
+        isFeedActive = false;
         isStatsActive =  false;
         isLibraryActive = true;
         isProfileActive = false;
       });
-    } else if (index == 2) {
+    } else if (index == 3) {
       setState(() {
         activeScreen = searchPage;
+        isFeedActive = false;
         isStatsActive =  false;
         isLibraryActive = false;
         isProfileActive = true;
@@ -96,28 +113,47 @@ class MainPageState extends State<MainPage> {
         Row(
           children: [
             IconButton(
-              color: isStatsActive ? Colors.green : Colors.grey,
-              icon: Icon(Icons.query_stats_rounded), 
-              iconSize: isStatsActive ? 32 : 27,
+              color: isFeedActive ? Colors.green : Colors.grey,
+              icon: Icon(Icons.feed), 
+              iconSize: isFeedActive ? 32 : 27,
               onPressed: (() => {setPage(0)})
             ), 
             IconButton(
-              color: isLibraryActive ? Colors.green : Colors.grey,
-              icon: Icon(Icons.library_books_rounded), 
-              iconSize: isLibraryActive ? 32 : 27,
+              color: isStatsActive ? Colors.green : Colors.grey,
+              icon: Icon(Icons.query_stats_rounded), 
+              iconSize: isStatsActive ? 32 : 27,
               onPressed: (() => {setPage(1)})
+            ), 
+            IconButton(
+              color: isLibraryActive ? Colors.green : Colors.grey,
+              icon: Icon(Icons.music_note), 
+              iconSize: isLibraryActive ? 32 : 27,
+              onPressed: (() => {setPage(2)})
             ), 
             IconButton(
               color: isProfileActive ? Colors.green : Colors.grey,
               icon: Icon(Icons.search_rounded), 
               iconSize: isProfileActive ? 32 : 27,
-              onPressed: (() => {setPage(2)})
+              onPressed: (() => {setPage(3)})
             ), 
           ],
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         ),
         color: Color.fromARGB(255, 20, 20, 20)
       ),
+      floatingActionButton: isFeedActive ? Container(
+        height: 50.0,
+        width: 50.0,
+        child: FittedBox(
+          child: FloatingActionButton(
+            child: Icon(Icons.add, color: Colors.black),
+            onPressed: () {}),
+        ),
+      ) : null,
+      floatingActionButtonLocation: CustomFloatingActionButtonLocation(
+              168,
+              725
+      )
     );
   }
 }
