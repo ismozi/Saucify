1fa54ce1bafd8856007dc506f9a5de3b2c12d35e
ismozi <61505183+ismozi@users.noreply.github.com>
2022-09-22 22:53:07 -0400

    Changed mixed playlist algo to avoid the same songs

--- firebase_functions/functions/src/index.ts
+++ firebase_functions/functions/src/index.ts
@@ -15,19 +15,33 @@ export const generateFirstMixes = functions.https.onRequest(async (req, res)=> {
     let remaining: number = playlistSize - (numOfTracks * following.length);
 
     for (const timeRange of timeRanges) {
+        let tracksIncluded: Array<String> = [];
         let tracksItems: Array<Object> = [];
 
         for (const [index, userId] of following.entries()) { 
             let userSnap = await db.collection("users").doc(userId).get();
             let userData = userSnap.data();
             let topTracks: Array<String> = userData!["topTracks"][timeRange];
+            let choosenTracks: Array<String> = []; 
 
-            if (index == following.length - 1)
-                topTracks = topTracks.slice(0, numOfTracks + remaining);
-            else 
-                topTracks = topTracks.slice(0, numOfTracks);
+            if (index == following.length - 1) {
+                let i = 0;
+                while (choosenTracks.length < numOfTracks + remaining && i < topTracks.length) {
+                    if (!tracksIncluded.includes(topTracks[i]))
+                        choosenTracks.push(topTracks[i]);
+                    i++;
+                }
+            } else {
+                let i = 0;
+                while (choosenTracks.length < numOfTracks && i < topTracks.length) {
+                    if (!tracksIncluded.includes(topTracks[i]))
+                        choosenTracks.push(topTracks[i]);
+                    i++;
+                }
+            }
 
-            topTracks.forEach((element) => {
+            choosenTracks.forEach((element) => {
+                tracksIncluded.push(element);
                 tracksItems.push({
                     "trackId": element,
                     "userId": userId
@@ -35,12 +49,30 @@ export const generateFirstMixes = functions.https.onRequest(async (req, res)=> {
             });
         }
 
-        targetUserData!["Mixes"][timeRange] = tracksItems;
+        targetUserData!["Mixes"][timeRange] = shuffle(tracksItems);
         docRef.set(targetUserData!);
     }
 
     res.send("done");
 });
+
+function shuffle(array: Array<Object>) {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
 
 export const onFollowingChange = functions.https.onRequest((req, res) => {
 
